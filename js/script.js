39ad5dc828680beeca6a33a3dd1429ad70ee6891
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-04-12 16:51:33 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Created by: Lucas Osmond
+// Created on: March 2023
+// This file contains the JS functions for index.html 
+
+'use strict'
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function calculate () {
+  // input
+  const length = parseFloat(document.getElementById('length-of-pyramid').value)
+  const width = parseFloat(document.getElementById('width-of-pyramid').value)
+  const height = parseFloat(document.getElementById('height-of-pyramid').value)
+
+  // process
+  const volume = (length * width * height)  / 3 
+
+  // output
+  document.getElementById('area').innerHTML = 'volume is: ' + volume + ' cm³'
+}
